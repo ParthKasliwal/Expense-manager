9fb5959a05dc25f0556c015d5978a8f885be8694
ParthKasliwal <162095273+ParthKasliwal@users.noreply.github.com>
2025-04-07 14:08:51 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,140 +1,144 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { APIUrl, handleError, handleSuccess } from '../utils';
-import { ToastContainer } from 'react-toastify';
-import ExpenseTable from './ExpenseTable';
-import ExpenseDetails from './ExpenseDetails';
-import ExpenseForm from './ExpenseForm';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { APIUrl, handleError, handleSuccess } from "../utils";
+import { ToastContainer } from "react-toastify";
+import ExpenseTable from "./ExpenseTable";
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseForm from "./ExpenseForm";
 
 function Home() {
-    const [loggedInUser, setLoggedInUser] = useState('');
-    const [expenses, setExpenses] = useState([]);
-    const [incomeAmt, setIncomeAmt] = useState(0);
-    const [expenseAmt, setExpenseAmt] = useState(0);
+  const [loggedInUser, setLoggedInUser] = useState("");
+  const [expenses, setExpenses] = useState([]);
+  const [incomeAmt, setIncomeAmt] = useState(0);
+  const [expenseAmt, setExpenseAmt] = useState(0);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        setLoggedInUser(localStorage.getItem('loggedInUser'))
-    }, [])
+  useEffect(() => {
+    setLoggedInUser(localStorage.getItem("loggedInUser"));
+  }, []);
 
-    const handleLogout = (e) => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('loggedInUser');
-        handleSuccess('User Loggedout');
-        setTimeout(() => {
-            navigate('/login');
-        }, 1000)
+  const handleLogout = (e) => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("loggedInUser");
+    handleSuccess("User Loggedout");
+    setTimeout(() => {
+      navigate("/login");
+    }, 1000);
+  };
+  useEffect(() => {
+    const amounts = expenses.map((item) => item.amount);
+    const income = amounts
+      .filter((item) => item > 0)
+      .reduce((acc, item) => (acc += item), 0);
+    const exp =
+      amounts
+        .filter((item) => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1;
+    setIncomeAmt(income);
+    setExpenseAmt(exp);
+  }, [expenses]);
+
+  const deleteExpens = async (id) => {
+    try {
+      const url = `${APIUrl}/expenses/${id}`;
+      const headers = {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+        method: "DELETE",
+      };
+      const response = await fetch(url, headers);
+      if (response.status === 403) {
+        localStorage.removeItem("token");
+        navigate("/login");
+        return;
+      }
+      const result = await response.json();
+      handleSuccess(result?.message);
+      console.log("--result", result.data);
+      setExpenses(result.data);
+    } catch (err) {
+      handleError(err);
     }
-    useEffect(() => {
-        const amounts = expenses.map(item => item.amount);
-        const income = amounts.filter(item => item > 0)
-            .reduce((acc, item) => (acc += item), 0);
-        const exp = amounts.filter(item => item < 0)
-            .reduce((acc, item) => (acc += item), 0) * -1;
-        setIncomeAmt(income);
-        setExpenseAmt(exp);
-    }, [expenses])
+  };
 
-    const deleteExpens = async (id) => {
-        try {
-            const url = `${APIUrl}/expenses/${id}`;
-            const headers = {
-                headers: {
-                    'Authorization': localStorage.getItem('token')
-                },
-                method: "DELETE"
-            }
-            const response = await fetch(url, headers);
-            if (response.status === 403) {
-                localStorage.removeItem('token');
-                navigate('/login');
-                return
-            }
-            const result = await response.json();
-            handleSuccess(result?.message)
-            console.log('--result', result.data);
-            setExpenses(result.data);
-        } catch (err) {
-            handleError(err);
-        }
+  const fetchExpenses = async () => {
+    try {
+      const url = `${APIUrl}/expenses`;
+      const headers = {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      };
+      const response = await fetch(url, headers);
+      if (response.status === 403) {
+        localStorage.removeItem("token");
+        navigate("/login");
+        return;
+      }
+      const result = await response.json();
+      console.log("--result", result.data);
+      setExpenses(result.data);
+    } catch (err) {
+      handleError(err);
     }
+  };
 
-    const fetchExpenses = async () => {
-        try {
-            const url = `${APIUrl}/expenses`;
-            const headers = {
-                headers: {
-                    'Authorization': localStorage.getItem('token')
-                }
-            }
-            const response = await fetch(url, headers);
-            if (response.status === 403) {
-                localStorage.removeItem('token');
-                navigate('/login');
-                return
-            }
-            const result = await response.json();
-            console.log('--result', result.data);
-            setExpenses(result.data);
-        } catch (err) {
-            handleError(err);
-        }
+  const addTransaction = async (data) => {
+    try {
+      const url = `${APIUrl}/expenses`;
+      const headers = {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(data),
+      };
+      const response = await fetch(url, headers);
+      if (response.status === 403) {
+        localStorage.removeItem("token");
+        navigate("/login");
+        return;
+      }
+      const result = await response.json();
+      handleSuccess(result?.message);
+      console.log("--result", result.data);
+      setExpenses(result.data);
+    } catch (err) {
+      handleError(err);
     }
+  };
 
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
 
-
-    const addTransaction = async (data) => {
-        try {
-            const url = `${APIUrl}/expenses`;
-            const headers = {
-                headers: {
-                    'Authorization': localStorage.getItem('token'),
-                    'Content-Type': 'application/json'
-                },
-                method: "POST",
-                body: JSON.stringify(data)
-            }
-            const response = await fetch(url, headers);
-            if (response.status === 403) {
-                localStorage.removeItem('token');
-                navigate('/login');
-                return
-            }
-            const result = await response.json();
-            handleSuccess(result?.message)
-            console.log('--result', result.data);
-            setExpenses(result.data);
-        } catch (err) {
-            handleError(err);
-        }
-    }
-
-    useEffect(() => {
-        fetchExpenses()
-    }, [])
-
-    return (
-        <div>
-            <div className='user-section'>
-                <h1>Welcome {loggedInUser}</h1>
-                <button onClick={handleLogout}>Logout</button>
-            </div>
-            <ExpenseDetails
-                incomeAmt={incomeAmt}
-                expenseAmt={expenseAmt}
-            />
-
-            <ExpenseForm
-                addTransaction={addTransaction} />
-
-            <ExpenseTable
-                expenses={expenses}
-                deleteExpens={deleteExpens}
-            />
-            <ToastContainer />
+  return (
+    <div>
+      <div className="top-section">
+        <h1>
+          Welcome <span>{loggedInUser}</span>
+        </h1>
+        <button onClick={handleLogout} className="logout-btn">
+          Logout
+        </button>
+      </div>
+      <div className="container">
+        {/* Left Section - Expense Form & Details */}
+        <div className="left-section">
+          <ExpenseDetails incomeAmt={incomeAmt} expenseAmt={expenseAmt} />
+          <ExpenseForm addTransaction={addTransaction} />
         </div>
-    )
+
+        {/* Right Section - Expense List */}
+        <div className="right-section">
+          <ExpenseTable expenses={expenses} deleteExpense={deleteExpens} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
