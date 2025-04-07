0770fc7a307e0645f5cd7a6d21b525268faba68f
ParthKasliwal <162095273+ParthKasliwal@users.noreply.github.com>
2025-04-07 14:08:31 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -57,7 +57,7 @@ function Login() {
     }
 
     return (
-        <div className='container'>
+        <div className='container1'>
             <h1>Login</h1>
             <form onSubmit={handleLogin}>
                 <div>
