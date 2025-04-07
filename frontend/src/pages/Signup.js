d5a49b0acff9d2b0f3c561498382a372f5ade0c8
ParthKasliwal <162095273+ParthKasliwal@users.noreply.github.com>
2025-04-07 14:07:41 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -54,7 +54,7 @@ function Signup() {
         }
     }
     return (
-        <div className='container'>
+        <div className='container1'>
             <h1>Signup</h1>
             <form onSubmit={handleSignup}>
                 <div>
